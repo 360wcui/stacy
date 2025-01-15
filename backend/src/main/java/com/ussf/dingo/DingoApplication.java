d541cd08d5b790d54477b7717a57715ca81d10e5
wcui <360wcui@gmail.com>
2025-01-15 17:51:20 -0500

    added initial backend

---
+++ backend/src/main/java/com/ussf/dingo/DingoApplication.java
@@ -0,0 +1,13 @@
+package com.ussf.dingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DingoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ussf/dingo/DingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ussf.dingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
