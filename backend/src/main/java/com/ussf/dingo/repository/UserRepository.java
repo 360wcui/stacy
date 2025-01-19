cd8477b473d75915be780fad22198a69acf2e3cd
wcui <360wcui@gmail.com>
2025-01-19 15:33:32 -0500

    update findByUsername to return Optional<User>

--- backend/src/main/java/com/ussf/dingo/repository/UserRepository.java
+++ backend/src/main/java/com/ussf/dingo/repository/UserRepository.java
@@ -5,7 +5,8 @@ import com.ussf.dingo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
