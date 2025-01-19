608d0233cb369b270194df102e173f1c8717ebc3
wcui <360wcui@gmail.com>
2025-01-19 15:39:24 -0500

    change the signing key must be 512 bits (64 bytes) to comply with the JWT JWA Specification.

--- backend/src/main/java/com/ussf/dingo/security/JwtUtils.java
+++ backend/src/main/java/com/ussf/dingo/security/JwtUtils.java
@@ -2,26 +2,27 @@ package com.ussf.dingo.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
-
 @Component
 public class JwtUtils {
 
-    private String jwtSecret = "yourSecretKey";  // Store in application.properties for better security
+    private static final SecretKey jwtSecretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
     private int jwtExpirationMs = 86400000; // 1 day expiration
 
     public String generateJwtToken(Authentication authentication) {
-
-        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
 
         return Jwts.builder()
-                .setSubject(userPrincipal.getUsername())
+                .setSubject(userDetails.getUsername())
+                .claim("roles", userDetails.getAuthorities()) // Add claims as needed
                 .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(jwtSecretKey, SignatureAlgorithm.HS512)
                 .compact();
     }
 }
