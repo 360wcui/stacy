208d63d4368a0693463a512d24fd7fadd80c9c8d
wcui <360wcui@gmail.com>
2025-01-19 15:32:49 -0500

    fixed a bug that returns wrong password

--- backend/src/main/java/com/ussf/dingo/security/UserDetailsImpl.java
+++ backend/src/main/java/com/ussf/dingo/security/UserDetailsImpl.java
@@ -1,6 +1,7 @@
 package com.ussf.dingo.security;
 
 import com.ussf.dingo.model.User;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,9 +9,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.Collections;
 
+@Getter
 public class UserDetailsImpl implements UserDetails {
 
-    private User user;
+    // Add getters for custom fields if needed
+    private final User user;
+
     public UserDetailsImpl(User user) {
         this.user = user;
     }
@@ -22,11 +26,32 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public String getPassword() {
-        return user.getUsername();
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
         return user.getUsername();
     }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
